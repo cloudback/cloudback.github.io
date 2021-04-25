b86359898cc0ea2b0eb2555a4214a6ae601760d8
Evgeniy Kosjakov (via Travis CI) <42_@mail.ru>
2021-04-25 20:00:02 +0000

    Deploy cloudback/docs to github.com/cloudback/cloudback.github.io.git:master

--- en.search.min.13630c470020035ecea289065eca2380a948cf241d7a383d04d50a2c13353a53.js
+++ en.search.min.13630c470020035ecea289065eca2380a948cf241d7a383d04d50a2c13353a53.js
@@ -2,7 +2,7 @@
 const characterPressed=String.fromCharCode(e.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();e.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.0f6559117bf771574258bdebe16d9f2bfbd2c794e2452dc5d137ac2b0483809f.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.07243c38766b51ef011d1f27be73b05392727fb3b33630f6a1a84772e8cc8551.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
