846025df31f1735fd2aaa6a98b29d55cb2832a66
Victor Vorobyev (via Travis CI) <victor_vorobyev@epam.com>
2021-05-25 21:24:31 +0000

    Deploy cloudback/docs to github.com/cloudback/cloudback.github.io.git:master

--- en.search.min.2815a7ec5ca3f748e86cc4d0c75c62087a74498bf1fa269dfaa564a18adcac7e.js
+++ en.search.min.2815a7ec5ca3f748e86cc4d0c75c62087a74498bf1fa269dfaa564a18adcac7e.js
@@ -2,7 +2,7 @@
 const characterPressed=String.fromCharCode(e.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();e.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.1c9b5e7aecbdaaacf24e5de984771a0a663eb535e4afd41abbf535c05557f02d.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.601dfe207ed4382102e69da9c0c309b83655475f6c7562e2376d02953d425762.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
